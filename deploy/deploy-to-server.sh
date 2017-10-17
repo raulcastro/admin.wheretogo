8f6b7744c9b2f60c8188758443acb5ad3c8ab3ab
Raul Castro <Raul Castro>
2017-10-17 18:34:19 -0500

    deploy to server

---
+++ deploy/deploy-to-server.sh
@@ -0,0 +1,1 @@
+rsync --progress -rvz /var/www/admin.wheretogo_root/* wheretogo.com.mx@s161140.gridserver.com:/home/161140/users/.home/domains/admin.wheretogo.com.mx/html --exclude-from rsync_exclude.txt
